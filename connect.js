315f1a04f76e665a80c41117b11d386811cb50d7
DownDiggity <83378036+DownDiggity@users.noreply.github.com>
2024-12-04 17:52:53 -0800

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,8 +1,10 @@
 import sqlite3 from 'sqlite3';
 const sql3 = sqlite3.verbose();
 
+//creates the connection to the sqlite database
 const DB = new sql3.Database('./store.db', sqlite3.OPEN_READWRITE, connected);
 
+//lets us know if the connection is succesfull
 function connected(err) {
   if (err) {
     console.log(err.message);
