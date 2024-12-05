bf9694ad785fdcdb9de45460e2b53da4a860e789
DownDiggity <83378036+DownDiggity@users.noreply.github.com>
2024-12-04 17:28:22 -0800

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,8 +1,6 @@
 import sqlite3 from 'sqlite3';
 const sql3 = sqlite3.verbose();
 
-// const DB = new sql3.Database(':memory:', sqlite3.OPEN_READWRITE, connected);
-// const DB = new sql3.Database('', sqlite3.OPEN_READWRITE, connected);
 const DB = new sql3.Database('./store.db', sqlite3.OPEN_READWRITE, connected);
 
 function connected(err) {
@@ -10,21 +8,8 @@ function connected(err) {
     console.log(err.message);
     return;
   }
-  console.log('Created the DB or SQLite DB does already exist');
+  console.log('Connected to DB');
 }
 
-let sql = `CREATE TABLE IF NOT EXISTS users(
-  id INTEGER PRIMARY KEY,
-  email TEXT NOT NULL,
-  password TEXT NOT NULL
-)`;
-DB.run(sql, [], (err) => {
-  //callback function
-  if (err) {
-    console.log('error creating enemies table');
-    return;
-  }
-  console.log('CREATED TABLE');
-});
-
 export { DB };
+
